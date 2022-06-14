bb4d837342e8bfb308aabafd7feb6d217d97467f
TrevorBlythe <77030926+TrevorBlythe@users.noreply.github.com>
2022-06-13 22:24:56 -0500

    Update ConvLayer.js

--- ConvLayer.js
+++ ConvLayer.js
@@ -25,12 +25,21 @@ this.filterw
 
 */
 
+/*
+if you wanna input an image do it like this.
+[r,r,r,r,g,g,g,g,b,b,b,b,b]
+NOT like this:
+[r,g,b,r,g,b,r,g,b,r,g,b]
 
+*/
 
 	class ConvLayer {
 
 		constructor(inWidth, inHeight, inDepth, filterWidth, filterHeight, filters=3, stride=1, padding=0) {
-
+				if(padding != 0){
+					throw "Dear user, I have not implemented padding yet.. set it to zero to avoid this message. " +
+								"Star the project if you want me to complete it. or send me 5 bucks ill do it right now.";
+				}
 				this.filters = filters;//the amount of filters
 				this.inWidth = inWidth;
 				this.inHeight = inHeight;
@@ -42,18 +51,14 @@ this.filterw
 				this.filterw = new Float32Array(filters * inDepth * filterWidth * filterHeight);
 				this.filterws = new Float32Array(filters * inDepth * filterWidth * filterHeight);
 				this.trainIterations = 0; 
-				this.outData = new Float32Array(((inWidth-filterWidth+2*padding)/stride+1) * ((inWidth-filterWidth+2*padding)/stride+1) * this.filters);
+				this.outData = new Float32Array(Math.ceil((inWidth-filterWidth+2*padding + 1)/stride) * Math.ceil((inHeight-filterHeight+2*padding + 1)/stride) * this.filters);
 				this.inData = new Float32Array(inWidth * inHeight * inDepth);
 				this.costs = new Float32Array(inWidth * inHeight * inDepth);
 				this.b = new Float32Array(this.outData.length);
 				this.bs = new Float32Array(this.outData.length);
-				this.accessed = new Float32Array(this.inData.length);
-				this.waccessed = new Float32Array(this.filterw.length);
+				this.accessed = new Float32Array(this.inData.length).fill(1);
 				if(this.filterWidth > inWidth + padding || this.filterHeight > inHeight + padding){
 					throw "Conv layer error: filters cannot be bigger than the input";
-				}
-				if((this.inWidth / stride) % 1 != 0 || (this.inHeight / stride) % 1 != 0){
-					console.log("WARNING: stride parameter should be a factor of input width and input height. or else it might not work properly")
 				}
 				//init random weights
 				for(var i = 0;i<this.filterw.length;i++){
@@ -65,6 +70,18 @@ this.filterw
 				for(var i = 0;i<this.b.length;i++){
 					this.b[i] = 1 * Math.random() * (Math.random() > 0.5 ? -1 : 1);
 				}
+
+				//Everything below here is precalculated constants used in forward/backward
+				//to optimize this and make sure we are as effeiciant as possible.
+				//DONT CHANGE THESE OR BIG BREAKY BREAKY!
+
+				this.hMFHPO = Math.ceil((this.inHeight - this.filterHeight + 1)/this.stride);
+				this.wMFWPO = Math.ceil((this.inWidth - this.filterWidth + 1)/this.stride);
+				this.hMFWMF = this.hMFHPO * this.wMFWPO;
+				this.wIH = this.inWidth * this.inHeight;
+				this.wIHID = this.inWidth * this.inHeight * this.inDepth;
+				this.fWIH = this.filterWidth * this.filterHeight;
+				this.fWIHID = this.inDepth * this.filterHeight * this.filterWidth;
 		}
 
 		inSize(){
@@ -75,34 +92,48 @@ this.filterw
 			return this.outData.length;
 		}
 
+		inSizeDimensions(){
+			return [this.inWidth,this.inHeight,this.inDepth];
+		}
+
+		outSizeDimensions(){
+			return[Math.ceil((this.inWidth-this.filterWidth+2*this.padding + 1)/this.stride),Math.ceil((this.inHeight-this.filterHeight+2*this.padding + 1)/this.stride),this.filters]
+		}
+
 		forward(inData){
 					if(inData){
 						if(inData.length != this.inSize()){
-							throw "INPUT SIZE WRONG ON CONV LAYER:\nexpected size ("
-							 + this.inSize() + "), got: (" + inData.length + ")";
+							throw "INPUT SIZE WRONG ON CONV LAYER:\nexpected array size ("
+							 + this.inSize() + ", dimensions: [" + this.inSizeDimensions() + "]), got: (" + inData.length + ")";
 						}
 						for(var i = 0;i<inData.length;i++){
 							this.inData[i] = inData[i];
 						}
 					}
 					this.outData.fill(0);
-	
+
 					for(var i = 0;i<this.filters;i++){
-						for(var g = 0;g<this.inHeight - this.filterHeight + 1;g++){
-							for(var b = 0;b<this.inWidth - this.filterWidth + 1;b++){
-								let odi = b + (g * (this.inWidth - this.filterWidth + 1)) + (i * (this.inWidth - this.filterWidth + 1) * (this.inHeight - this.filterHeight + 1));
+						const iHMFWMF = i * this.hMFWMF;
+						const iFWIHID = i * this.fWIHID;
+						for(var g = 0;g<this.hMFHPO;g++){
+							const ga = g * this.stride
+							const gWMFWPO = g * this.wMFWPO;
+							for(var b = 0;b<this.wMFWPO;b++){
+								const odi = b + gWMFWPO + iHMFWMF;
+								const ba = b * this.stride;
 								for(var h = 0;h<this.inDepth;h++){
+									const hWIH = h * this.wIH + ba;
+									const hFWIH = h * this.fWIH + iFWIHID;
 									for(var j = 0;j<this.filterHeight;j++){
+										const jGAIWBA = (j+ga) * this.inWidth + hWIH;
+										const jFWHFWIH = (j * this.filterWidth) + hFWIH
 										for(var k = 0;k<this.filterWidth;k++){
 											this.outData[odi] += 
-											this.inData[(b+k) + ((j+g)*this.inWidth) + (h * this.inWidth * this.inHeight)] * 
-											this.filterw[i * this.inDepth * this.filterWidth * this.filterHeight + k + (j * this.filterWidth) + (h * this.filterWidth * this.filterHeight)];
+											this.inData[k + jGAIWBA] * 
+											this.filterw[k+jFWHFWIH];
 										}
 									}
 								}
-								// this.outData[b + (g * (this.inWidth - this.filterWidth + 1)) + (i * (this.inWidth - this.filterWidth + 1) * (this.inHeight - this.filterHeight + 1))] /= this.inDepth * this.filterHeight * this.filterWidth;
-								//pick one?
-								// this.outData[b + (g * (this.inWidth - this.filterWidth + 1)) + (i * (this.inWidth - this.filterWidth + 1) * (this.inHeight - this.filterHeight + 1))] /= this.inDepth
 								this.outData[odi] += this.b[odi];
 							}
 						}
@@ -120,46 +151,28 @@ this.filterw
 				if(this.nextLayer == undefined){
 					throw "error backproping on an unconnected layer with no expected parameter input"
 				}
-				
-				for(var i = 0;i<this.filters;i++){
-					for(var g = 0;g<this.inHeight - this.filterHeight + 1;g++){
-						for(var b = 0;b<this.inWidth - this.filterWidth + 1;b++){
-							let odi = b + (g * (this.inWidth - this.filterWidth + 1)) + (i * (this.inWidth - this.filterWidth + 1) * (this.inHeight - this.filterHeight + 1));
-							let err = this.nextLayer.costs[odi];
-							for(var h = 0;h<this.inDepth;h++){
-								for(var j = 0;j<this.filterHeight;j++){
-									for(var k = 0;k<this.filterWidth;k++){
-										this.costs[(b+k) + ((j+g)*this.inWidth) + (h * this.inWidth * this.inHeight)] +=
-										this.filterw[i * this.inDepth * this.filterWidth * this.filterHeight + k + (j * this.filterWidth) + (h * this.filterWidth * this.filterHeight)] * err;
-										this.waccessed[i * this.inDepth * this.filterWidth * this.filterHeight + k + (j * this.filterWidth) + (h * this.filterWidth * this.filterHeight)] += 1;
-										this.accessed[(b+k) + ((j+g)*this.inWidth) + (h * this.inWidth * this.inHeight)] += 1;
-										this.filterws[i * this.inDepth * this.filterWidth * this.filterHeight + k + (j * this.filterWidth) + (h * this.filterWidth * this.filterHeight)] += 
-										this.inData[(b+k) + ((j+g)*this.inWidth) + (h * this.inWidth * this.inHeight)] * err;
-									}
-								}
-							}
-							this.bs[odi] += err;
-						}
-					}
-				}
-
-			}else{
-
+			}
 			
-				for(var i = 0;i<this.filters;i++){
-					for(var g = 0;g<this.inHeight - this.filterHeight + 1;g++){
-						for(var b = 0;b<this.inWidth - this.filterWidth + 1;b++){
-							let odi = b + (g * (this.inWidth - this.filterWidth + 1)) + (i * (this.inWidth - this.filterWidth + 1) * (this.inHeight - this.filterHeight + 1));
-							let err = (expected[odi] - this.outData[odi]);
-							for(var h = 0;h<this.inDepth;h++){
-								for(var j = 0;j<this.filterHeight;j++){
-									for(var k = 0;k<this.filterWidth;k++){
-										this.costs[(b+k) + ((j+g)*this.inWidth) + (h * this.inWidth * this.inHeight)] +=
-										this.filterw[i * this.inDepth * this.filterWidth * this.filterHeight + k + (j * this.filterWidth) + (h * this.filterWidth * this.filterHeight)] * err;
-										this.waccessed[i * this.inDepth * this.filterWidth * this.filterHeight + k + (j * this.filterWidth) + (h * this.filterWidth * this.filterHeight)] += 1;
-										this.accessed[(b+k) + ((j+g)*this.inWidth) + (h * this.inWidth * this.inHeight)] += 1;
-										this.filterws[i * this.inDepth * this.filterWidth * this.filterHeight + k + (j * this.filterWidth) + (h * this.filterWidth * this.filterHeight)] += 
-										this.inData[(b+k) + ((j+g)*this.inWidth) + (h * this.inWidth * this.inHeight)] * err;
+			for(var i = 0;i<this.filters;i++){
+				const iHMFWMF = i * this.hMFWMF;
+				const iFWIHID = i * this.fWIHID;
+				for(var g = 0;g<this.hMFHPO;g++){
+					const ga = g * this.stride
+					const gWMFWPO = g * this.wMFWPO;
+					for(var b = 0;b<this.wMFWPO;b++){
+						const odi = b + gWMFWPO + iHMFWMF;
+						const ba = b * this.stride;
+						let err = (!expected ? this.nextLayer.costs[odi]:expected[odi] - this.outData[odi]);
+						for(var h = 0;h<this.inDepth;h++){
+							const hWIH = h * this.wIH;
+							const hFWIH = h * this.fWIH + iFWIHID;
+							for(var j = 0;j<this.filterHeight;j++){
+								const jGAIWBA = (j+ga) * this.inWidth + hWIH + ba;
+								const jFWHFWIH = (j * this.filterWidth) + hFWIH
+								for(var k = 0;k<this.filterWidth;k++){
+										this.costs[k + jGAIWBA] += this.filterw[k+jFWHFWIH] * err;
+										this.accessed[k + jGAIWBA]++;
+										this.filterws[k+jFWHFWIH] += this.inData[k + jGAIWBA] * err;
 									}
 								}
 							}
@@ -168,19 +181,16 @@ this.filterw
 					}
 				}
 
-			}// end of 'sometimes the most effeciant is the least elagant' ----
-
+			//Do i need this or not wtf??
 			for(var i = 0;i<this.inSize();i++){
 				this.costs[i] = this.costs[i] / this.accessed[i];
-				this.accessed[i] = 0;
+				this.accessed[i] = 1;
 			}
 		}
 
 		updateParams(lr){
-			console.log('updating params');
 			for(var i = 0 ; i < this.filterw.length; i++){
-				this.filterws[i] /=  this.waccessed[i];
-				this.waccessed[i] = 0;
+				this.filterws[i] /=  this.outSize() / this.filters;
 				this.filterw[i] += this.filterws[i] * lr;
 				this.trainIterations = 0;
 			}
