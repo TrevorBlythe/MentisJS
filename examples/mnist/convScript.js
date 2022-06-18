6bce9162ce49600e6ba8a3eab22858df9706657d
TrevorBlythe <77030926+TrevorBlythe@users.noreply.github.com>
2022-06-17 20:07:26 -0500

    forgor to udate this part

--- examples/mnist/convScript.js
+++ examples/mnist/convScript.js
@@ -6,129 +6,134 @@ let clear = function () {
 	document.getElementById('out').innerHTML = '';
 };
 
-let ctxZ = document.getElementById('canvas0').getContext('2d');
-let ctxO = document.getElementById('canvas1').getContext('2d');
-let ctxT = document.getElementById('canvas2').getContext('2d');
-let ctxTh = document.getElementById('canvas3').getContext('2d');
+let examplesSeen = 0;
 
-let drawFromArr = function (arr, ctx) {
+let ctx = [
+	document.getElementById('canvas0').getContext('2d'),
+	document.getElementById('canvas1').getContext('2d'),
+	document.getElementById('canvas2').getContext('2d'),
+	document.getElementById('canvas3').getContext('2d'),
+	document.getElementById('canvas4').getContext('2d'),
+	document.getElementById('canvas5').getContext('2d'),
+	document.getElementById('canvas6').getContext('2d'),
+	document.getElementById('canvas7').getContext('2d'),
+	document.getElementById('canvas8').getContext('2d'),
+	document.getElementById('canvas9').getContext('2d'),
+];
+
+let ctxF = document.getElementById('filterCanvas').getContext('2d');
+let ctxFG = document.getElementById('filterGCanvas').getContext('2d');
+
+let drawFromArr = function (arr, ctxy) {
 	let width = 28;
 	let height = 28;
 	for (var i = 0; i < height; i++) {
 		for (var j = 0; j < width; j++) {
 			let x = arr[i * width + j] * 255;
-			ctx.fillStyle = 'rgb(' + x + ',' + x + ',' + x + ')';
-			ctx.fillRect(j * 5, i * 5, 5, 5);
+			ctxy.fillStyle = 'rgb(' + x + ',' + x + ',' + x + ')';
+			ctxy.fillRect(j * 5, i * 5, 5, 5);
+		}
+	}
+};
+
+let drawFromArrF = function (arr, ctx, xx, y, outOf) {
+	let width = net.layers[0].filterWidth;
+	let height = net.layers[0].filterHeight;
+	xx = xx || 0;
+	y = y || 0;
+	for (var i = 0; i < height; i++) {
+		for (var j = 0; j < width; j++) {
+			let x = arr[i * width + j] * outOf;
+
+			if (x < 0) {
+				ctx.fillStyle = 'rgb(' + -x + ',' + 0 + ',' + 0 + ')';
+			} else {
+				ctx.fillStyle = 'rgb(' + 0 + ',' + x + ',' + 0 + ')';
+			}
+			ctx.fillRect(xx + j * 5, y + i * 5, 5, 5);
 		}
 	}
 };
 
 let getDigit = function (digit) {
-	let c = Math.floor(Math.random() * 100);
+	let c = Math.floor(Math.random() * 49);
 	return digits[digit].slice(c * 28 * 28, 28 * 28 + c * 28 * 28);
 };
 
 let greenBox = function (p) {
-	return (
-		"<span style='margin-right:3%;display:inline-block;height:20px;width:20px;background-color:rgb(0," +
-		p +
-		",0);border:1px solid black'></span>"
-	);
+	return "<span style='display:inline-block;height:20px;width:20px;background-color:rgb(0," + p + ",0);border:1px solid black'></span>";
 };
 //inWidth, inHeight, inDepth, filterWidth, filterHeight, filters=3, stride=1, padding=0
+
+// var net = new swag.Net([
+// 	new swag.Conv(28, 28, 1, 4, 4, 3, 1, 0),
+// 	new swag.MaxPool(25, 25, 3, 5, 5, 5),
+// 	new swag.Conv(5, 5, 3, 2, 2, 3, 1, 0),
+// 	new swag.MaxPool(4, 4, 3, 2, 2, 1),
+// 	new swag.FC(27, 13),
+// 	new swag.Sig(13),
+// 	new swag.FC(13, 10),
+// 	new swag.Relu(10),
+// ]);
+
 var net = new swag.Net([
-	new swag.Conv(28, 28, 1, 4, 4, 3, 1, 0),
-	new swag.MaxPool(25, 25, 3, 5, 5, 5),
-	new swag.FC(75, 30),
-	new swag.Sig(30),
-	new swag.FC(30, 4),
-	new swag.Sig(4),
+	new swag.Conv(28, 28, 1, 10, 10, 5, 3, 0),
+	new swag.MaxPool(7, 7, 5, 2, 2, 2),
+	new swag.FC(45, 35),
+	new swag.Sig(35),
+	new swag.FC(35, 10),
+	new swag.Relu(10),
 ]);
 
-net.batchSize = 5;
+net.batchSize = 20;
 net.learningRate = 0.1;
 
-// let counter = 0;
-// setInterval(function(){
-// 	counter++;
-// 	drawFromArr(digits.three.slice(counter * 28 * 28,28 * 28 + counter * 28 * 28),28,28);
-// },100);
+let names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
-setInterval(function () {
-	for (var i = 0; i < 10; i++) {
-		let c = getDigit('three');
-		net.train(c, [0, 0, 0, 1]);
-		c = getDigit('two');
-		net.train(c, [0, 0, 1, 0]);
-		c = getDigit('one');
-		net.train(c, [0, 1, 0, 0]);
-		c = getDigit('zero');
-		net.train(c, [1, 0, 0, 0]);
-	}
-	let c = getDigit('three');
-	let err = net.train(c, [0, 0, 0, 1]);
-	document.getElementById('errTh').innerHTML =
-		'Total Error: ' + parseInt(err * 100) + '%';
-	document.getElementById('threeout').innerHTML =
-		'zero: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[0] * 255) +
-		' one: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[1] * 255) +
-		' two: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[2] * 255) +
-		' three: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[3] * 255) +
-		' ';
-	drawFromArr(c, ctxTh);
+var go = function () {
+	window.loop = setInterval(function () {
+		for (var i = 0; i < 10; i++) {
+			let c = Array(10).fill(0);
+			c[i] = 5;
+			let err = net.train(getDigit(names[i]), c);
+			document.getElementById('err' + names[i]).innerHTML = 'Total Error: ' + parseInt(err * 100) + '%';
+			document.getElementById(names[i] + 'out').innerHTML = '';
+			for (var j = 0; j < 10; j++) {
+				document.getElementById(names[i] + 'out').innerHTML += greenBox(net.layers[net.layers.length - 1].outData[j] * 255);
+			}
+			drawFromArr(getDigit(names[i]), ctx[i]);
+		}
+		//draw the filters
+		for (var i = 0; i < net.layers[0].filters; i++) {
+			ctxF.clearRect(i * net.layers[0].filterWidth * 20, 0, 100, 100);
+			drawFromArrF(
+				net.layers[0].filterw.slice(
+					i * net.layers[0].filterWidth * net.layers[0].filterHeight,
+					i * net.layers[0].filterWidth * net.layers[0].filterHeight + net.layers[0].filterWidth + net.layers[0].filterWidth * (net.layers[0].filterHeight - 1)
+				),
+				ctxF,
+				5 + i * net.layers[0].filterWidth * 6,
+				5,
+				255
+			);
+		}
 
-	c = getDigit('two');
-	err = net.train(c, [0, 0, 1, 0]);
-	document.getElementById('errT').innerHTML =
-		'Total Error: ' + parseInt(err * 100) + '%';
+		for (var i = 0; i < net.layers[0].filters; i++) {
+			ctxFG.clearRect(i * net.layers[0].filterWidth * 20, 0, 100, 100);
+			drawFromArrF(
+				net.layers[0].filterws.slice(
+					i * net.layers[0].filterWidth * net.layers[0].filterHeight,
+					i * net.layers[0].filterWidth * net.layers[0].filterHeight + net.layers[0].filterWidth + net.layers[0].filterWidth * (net.layers[0].filterHeight - 1)
+				),
+				ctxFG,
+				5 + i * net.layers[0].filterWidth * 6,
+				5,
+				2550
+			);
+		}
+		document.getElementById('es').innerHTML = 'examples seen:' + examplesSeen;
+		examplesSeen++;
+	}, 0);
+};
 
-	document.getElementById('twoout').innerHTML =
-		'zero: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[0] * 255) +
-		' one: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[1] * 255) +
-		' two: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[2] * 255) +
-		' three: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[3] * 255) +
-		' ';
-	drawFromArr(c, ctxT);
-
-	c = getDigit('one');
-	err = net.train(c, [0, 1, 0, 0]);
-	document.getElementById('errO').innerHTML =
-		'Total Error: ' + parseInt(err * 100) + '%';
-
-	document.getElementById('oneout').innerHTML =
-		'zero: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[0] * 255) +
-		' one: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[1] * 255) +
-		' two: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[2] * 255) +
-		' three: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[3] * 255) +
-		' ';
-	drawFromArr(c, ctxO);
-
-	c = getDigit('zero');
-	err = net.train(c, [1, 0, 0, 0]);
-	document.getElementById('errZ').innerHTML =
-		'Total Error: ' + parseInt(err * 100) + '%';
-
-	document.getElementById('zeroout').innerHTML =
-		'zero: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[0] * 255) +
-		' one: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[1] * 255) +
-		' two: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[2] * 255) +
-		' three: ' +
-		greenBox(net.layers[net.layers.length - 1].outData[3] * 255) +
-		' ';
-	drawFromArr(c, ctxZ);
-}, 100);
+go();
