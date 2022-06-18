6df013bb9247ecb32538b08007691d502b74d3a7
TrevorBlythe <77030926+TrevorBlythe@users.noreply.github.com>
2022-06-17 21:02:21 -0500

    Fixing oopsie

--- src/layers/ConvLayer.js
+++ src/layers/ConvLayer.js
@@ -1,153 +1,85 @@
 {
+	// [(W−K+2P)/S]+1.
+
+	// W is the input volume - in your case 128
+	// K is the Kernel size - in your case 5
+	// P is the padding - in your case 0 i believe
+	// S is the stride - which you have not provided.
+
+	//filters stored as such
+	//this.filters[] array of num
 	/*
-		Layer specification:
+if filter is 3x3 with indepth of 3 the first filter stored as such:
+this.filterw = [0,1,2,3,4,5,6,7,8,
+								0,1,2,3,4,5,6,7,8,
+								0,1,2,3,4,5,6,7,8]
 
-		Each layer needs an inData and outData array.
-		These arrays should never be replaced because they are shared objects.
-		This means never do this. "this.inData = [];" EXCEPT in the constructor.
-		Replacing the inData or outData array will de-connect the layers.
+Each filter has depth equal to the InDepth, thats why there are three 3x3 in the array.
 
-		Each layer needs a "forward" and a "backward" function. The forward function should
-		be able to take an input array as input or use the data in "this.inData." The same for the 
-		backward function, it should take an array of "expected" values or it should backpropogate
-		based on the cost of the next layer (this.nextLayer.costs).
+this.filterw
+[filterNum * this.inDepth * filterWidth * filterHeight + x + (y * filterWidth) + (depth * filterWidth * filterHeight)]
 
-		REQUIRED FUNCTIONS:
-			inSize()
-			outSize()
-			forward(inData::array) --returns outData
-			backward(inData::array) --returns loss of the layer
-			updateParams(optimizer::string) // optimizer examples: "SGD" "adam" "adagrad" "adadelta"
+*/
 
-		NOT REQUIRED FUNCTIONS:
-			inSizeDimensions() -- returns array of dimensions i.e. [28,28,3]
-			outSizeDimensions()
-		
-		REQUIRED PARAMETERS:
-			inData
-			outData
-			lr -- aka learning rate
-			costs
-			nextLayer
-			gpuEnabled
+	/*
+if you wanna input an image do it like this.
+[r,r,r,r,g,g,g,g,b,b,b,b,b]
+NOT like this:
+[r,g,b,r,g,b,r,g,b,r,g,b]
 
-		FUNCTIONS FOR EVOLOUTION BASED LEARNING:
-			mutate(mutationRate::float, mutationIntensity::float)
+*/
 
-		FUNCTIONS FOR GPU SUPPORT:
-			initGPU() -- this should at least make "this.gpuEnabled" true.
-
-			You can code the rest yourself, swagnet uses gpujs and swag.gpu is a 
-			global gpu object to use to prevent making multpile gpu objects. 
-			Make it work however you want.
-
-			If you have all this your layer should work inside a real net!
-
-		
-
-	*/
-
-	class FCLayer {
-		constructor(inSize, outSize) {
-			if (!inSize) {
-				throw 'Missing parameter in FC Constructor: (input size)';
+	class ConvLayer {
+		constructor(inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1, padding = 0) {
+			if (padding != 0) {
+				throw (
+					'Dear user, I have not implemented padding yet.. set it to zero to avoid this message. ' +
+					'Star the project if you want me to complete it. or send me 5 bucks ill do it right now.'
+				);
 			}
-
-			if (!outSize) {
-				throw 'Missing parameter in FC Constructor: (output size)';
-			}
-
 			this.lr = 0.01; //learning rate, this will be set by the Net object
-			//dont set it in the constructor unless you really want to
 
-			this.gpuEnabled = false;
-			this.trainIterations = 0; //++'d whenever backwards is called;
-			this.ws = new Float32Array(inSize * outSize); //the weights sensitivities to error
-			this.bs = new Float32Array(outSize); //the bias sensitivities to error
-			this.nextLayer; //the connected layer
-			this.inData = new Float32Array(inSize); //the inData
-			this.outData = new Float32Array(outSize);
-			this.w = new Float32Array(inSize * outSize); //this will store the weights
-			this.b = new Float32Array(outSize); //this will store the biases (biases are for the outData (next layer))
-			this.costs = new Float32Array(inSize); //costs for each neuron
-
-			for (var j = 0; j < inSize; j++) {
-				//----init random weights
-				for (var h = 0; h < outSize; h++) {
-					this.w[h + j * outSize] = 1 * Math.random() * (Math.random() > 0.5 ? -1 : 1);
-					this.ws[h + j * outSize] = 0;
-				}
-			} // ---- end init weights
-
-			for (var j = 0; j < outSize; j++) {
-				this.b[j] = 1 * Math.random() * (Math.random() > 0.5 ? -1 : 1);
-				this.bs[j] = 0;
-			} ///---------adding random biases
-		}
-
-		forward(inData) {
-			if (inData) {
-				if (inData.length != this.inSize()) {
-					throw 'INPUT SIZE WRONG ON FC LAYER:\nexpected size (' + this.inSize() + '), got: (' + inData.length + ')';
-				}
-				for (var i = 0; i < inData.length; i++) {
-					this.inData[i] = inData[i];
-				}
+			this.filters = filters; //the amount of filters
+			this.inWidth = inWidth;
+			this.inHeight = inHeight;
+			this.inDepth = inDepth;
+			this.filterWidth = filterWidth;
+			this.filterHeight = filterHeight;
+			this.stride = stride;
+			this.padding = padding;
+			this.filterw = new Float32Array(filters * inDepth * filterWidth * filterHeight);
+			this.filterws = new Float32Array(filters * inDepth * filterWidth * filterHeight);
+			this.trainIterations = 0;
+			this.outData = new Float32Array(
+				Math.ceil((inWidth - filterWidth + 2 * padding + 1) / stride) * Math.ceil((inHeight - filterHeight + 2 * padding + 1) / stride) * this.filters
+			);
+			this.inData = new Float32Array(inWidth * inHeight * inDepth);
+			this.costs = new Float32Array(inWidth * inHeight * inDepth);
+			// this.b = new Float32Array(this.outData.length);  bias in a conv layer is dumb
+			// this.bs = new Float32Array(this.outData.length);
+			// this.accessed = new Float32Array(this.inData.length).fill(1);
+			if (this.filterWidth > inWidth + padding || this.filterHeight > inHeight + padding) {
+				throw 'Conv layer error: filters cannot be bigger than the input';
 			}
-
-			for (var h = 0; h < this.outSize(); h++) {
-				this.outData[h] = 0; //reset outData activation
-				for (var j = 0; j < this.inSize(); j++) {
-					this.outData[h] += this.inData[j] * this.w[h + j * this.outSize()]; // the dirty deed
-				}
-				this.outData[h] += this.b[h];
+			//init random weights
+			for (var i = 0; i < this.filterw.length; i++) {
+				this.filterw[i] = 0.1 * Math.random() * (Math.random() > 0.5 ? -1 : 1);
 			}
-		}
+			// for (var i = 0; i < this.b.length; i++) {
+			// 	this.b[i] = 1 * Math.random() * (Math.random() > 0.5 ? -1 : 1);
+			// }
 
-		backward(expected) {
-			this.trainIterations++;
-			let loss = 0;
-			for (var i = 0; i < this.inSize(); i++) {
-				//reset the costs
-				this.costs[i] = 0;
-			}
+			//Everything below here is precalculated constants used in forward/backward
+			//to optimize this and make sure we are as effeiciant as possible.
+			//DONT CHANGE THESE OR BIG BREAKY BREAKY!
 
-			if (!expected) {
-				// -- sometimes the most effiecant way is the least elagant one...
-				if (this.nextLayer == undefined) {
-					throw 'error backproping on an unconnected layer with no expected parameter input';
-				}
-				for (var j = 0; j < this.outSize(); j++) {
-					let err = this.nextLayer.costs[j];
-					loss += Math.pow(err, 2);
-					for (var i = 0; i < this.inSize(); i++) {
-						//activation times error = change to the weight.
-						this.ws[j + i * this.outSize()] += this.inData[i] * err * 2;
-						this.costs[i] += this.w[j + i * this.outSize()] * err * 2;
-					}
-					//bias grad is real simple :)
-					this.bs[j] += err;
-				}
-			} else {
-				for (var j = 0; j < this.outSize(); j++) {
-					let err = expected[j] - this.outData[j];
-					loss += Math.pow(err, 2);
-					for (var i = 0; i < this.inSize(); i++) {
-						//activation times error = change to the weight.
-						this.ws[j + i * this.outSize()] += this.inData[i] * err * 2;
-						this.costs[i] += this.w[j + i * this.outSize()] * err * 2;
-					}
-					//bias grad is real simple :)
-					this.bs[j] += err;
-				}
-			} // end of 'sometimes the most effeciant is the least elagant' ----
-
-			//finish averaging the costs
-			for (var i = 0; i < this.inSize(); i++) {
-				this.costs[i] = this.costs[i] / this.outSize();
-			}
-
-			return loss / this.outSize();
+			this.hMFHPO = Math.ceil((this.inHeight - this.filterHeight + 1) / this.stride);
+			this.wMFWPO = Math.ceil((this.inWidth - this.filterWidth + 1) / this.stride);
+			this.hMFWMF = this.hMFHPO * this.wMFWPO;
+			this.wIH = this.inWidth * this.inHeight;
+			this.wIHID = this.inWidth * this.inHeight * this.inDepth;
+			this.fWIH = this.filterWidth * this.filterHeight;
+			this.fWIHID = this.inDepth * this.filterHeight * this.filterWidth;
 		}
 
 		inSize() {
@@ -158,62 +90,134 @@
 			return this.outData.length;
 		}
 
+		inSizeDimensions() {
+			return [this.inWidth, this.inHeight, this.inDepth];
+		}
+
+		outSizeDimensions() {
+			return [
+				Math.ceil((this.inWidth - this.filterWidth + 2 * this.padding + 1) / this.stride),
+				Math.ceil((this.inHeight - this.filterHeight + 2 * this.padding + 1) / this.stride),
+				this.filters,
+			];
+		}
+
+		forward(inData) {
+			if (inData) {
+				if (inData.length != this.inSize()) {
+					throw (
+						'INPUT SIZE WRONG ON CONV LAYER:\nexpected array size (' +
+						this.inSize() +
+						', dimensions: [' +
+						this.inSizeDimensions() +
+						']), got: (' +
+						inData.length +
+						')'
+					);
+				}
+				for (var i = 0; i < inData.length; i++) {
+					this.inData[i] = inData[i];
+				}
+			}
+			this.outData.fill(0);
+
+			for (var i = 0; i < this.filters; i++) {
+				const iHMFWMF = i * this.hMFWMF;
+				const iFWIHID = i * this.fWIHID;
+				for (var g = 0; g < this.hMFHPO; g++) {
+					const ga = g * this.stride;
+					const gWMFWPO = g * this.wMFWPO;
+					for (var b = 0; b < this.wMFWPO; b++) {
+						const odi = b + gWMFWPO + iHMFWMF;
+						const ba = b * this.stride;
+						for (var h = 0; h < this.inDepth; h++) {
+							const hWIH = h * this.wIH + ba;
+							const hFWIH = h * this.fWIH + iFWIHID;
+							for (var j = 0; j < this.filterHeight; j++) {
+								const jGAIWBA = (j + ga) * this.inWidth + hWIH;
+								const jFWHFWIH = j * this.filterWidth + hFWIH;
+								for (var k = 0; k < this.filterWidth; k++) {
+									this.outData[odi] += this.inData[k + jGAIWBA] * this.filterw[k + jFWHFWIH];
+								}
+							}
+						}
+						// this.outData[odi] += this.b[odi];
+					}
+				}
+			}
+		}
+
+		backward(expected) {
+			let loss = 0;
+			this.trainIterations++;
+			for (var i = 0; i < this.inSize(); i++) {
+				//reset the costs
+				this.costs[i] = 0;
+			}
+
+			if (!expected) {
+				// -- sometimes the most effiecant way is the least elagant one...
+				if (this.nextLayer == undefined) {
+					throw 'error backproping on an unconnected layer with no expected parameter input';
+				}
+			}
+
+			for (var i = 0; i < this.filters; i++) {
+				const iHMFWMF = i * this.hMFWMF;
+				const iFWIHID = i * this.fWIHID;
+				for (var g = 0; g < this.hMFHPO; g++) {
+					const ga = g * this.stride;
+					const gWMFWPO = g * this.wMFWPO;
+					for (var b = 0; b < this.wMFWPO; b++) {
+						const odi = b + gWMFWPO + iHMFWMF;
+						const ba = b * this.stride;
+						let err = !expected ? this.nextLayer.costs[odi] : expected[odi] - this.outData[odi];
+						loss += Math.pow(err, 2);
+						for (var h = 0; h < this.inDepth; h++) {
+							const hWIH = h * this.wIH;
+							const hFWIH = h * this.fWIH + iFWIHID;
+							for (var j = 0; j < this.filterHeight; j++) {
+								const jGAIWBA = (j + ga) * this.inWidth + hWIH + ba;
+								const jFWHFWIH = j * this.filterWidth + hFWIH;
+								for (var k = 0; k < this.filterWidth; k++) {
+									this.costs[k + jGAIWBA] += this.filterw[k + jFWHFWIH] * err;
+									// this.accessed[k + jGAIWBA]++;
+									this.filterws[k + jFWHFWIH] += this.inData[k + jGAIWBA] * err;
+								}
+							}
+						}
+						// this.bs[odi] += err;
+					}
+				}
+			}
+
+			// for (var i = 0; i < this.inSize(); i++) {
+			// 	this.costs[i] =
+			// 		this.costs[i] / (this.accessed[i] > 0 ? this.accessed[i] : 0);
+			// 	this.accessed[i] = 0;
+			// }
+
+			return loss / (this.wMFWPO * this.hMFHPO * this.filters);
+		}
+
 		updateParams(optimizer) {
-			if (this.trainIterations < 0) {
-				return;
+			for (var i = 0; i < this.filterw.length; i++) {
+				// this.filterws[i] /= this.outSize() / this.filters;
+				// this.filterws[i] /= this.trainIterations;
+				this.filterw[i] += this.filterws[i] * this.lr;
+				this.filterws[i] = 0;
 			}
-			if (optimizer == 'SGD') {
-				for (var i = 0; i < this.ws.length; i++) {
-					this.ws[i] /= this.trainIterations;
-					this.w[i] += this.ws[i] * this.lr;
-					this.ws[i] = 0;
-				}
-
-				for (var j = 0; j < this.outSize(); j++) {
-					this.bs[j] /= this.trainIterations;
-					this.b[j] += this.bs[j] * this.lr;
-					this.bs[j] = 0;
-				}
-
-				this.trainIterations = 0;
-			}
+			//i forgor to update bias
+			// for (var i = 0; i < this.b.length; i++) {
+			// 	//is this correct i wonder?
+			// 	this.bs[i] /= this.wMFWPO * this.hMFHPO * this.filters;
+			// 	this.b[i] += this.bs[i] * this.lr;
+			// 	this.bs[i] = 0;
+			// }
+			this.trainIterations = 0;
 		}
-
-		mutate(mutationRate, mutationIntensity) {
-			for (var i = 0; i < this.w.length; i++) {
-				if (Math.random() < mutationRate) {
-					this.w[i] += Math.random() * mutationIntensity * (Math.random() > 0.5 ? -1 : 1);
-				}
-			}
-			for (var i = 0; i < this.b.length; i++) {
-				if (Math.random() < mutationRate) {
-					this.b[i] += Math.random() * mutationIntensity * (Math.random() > 0.5 ? -1 : 1);
-				}
-			}
-		}
-
-		//----------------
-		// GPU specific stuff below! Beware.
-		//----------------
-
-		// initGPU(){
-		// 	this.forwardGPUKernel = swag.gpu.createKernel(function(inData, weights, biases, outDataLength, inDataLength) {
-		// 		var act = 0;
-		// 		for (var j = 0; j < inDataLength; j++) {
-		// 					act +=
-		// 						inData[j] *
-		// 						weights[this.thread.x + j *	outDataLength];// the dirty deed
-		// 		}
-		// 		act += biases[this.thread.x];
-		// 		return act
-		// 	}).setOutput([this.outData.length]);
-		// }
-
-		//----------------
-		// End of GPU specific stuff. Take a breather.
-		//----------------
 	}
 
-	swag.FCLayer = FCLayer;
-	swag.FC = FCLayer;
+	swag.ConvLayer = ConvLayer;
+	swag.Conv = ConvLayer;
 }
