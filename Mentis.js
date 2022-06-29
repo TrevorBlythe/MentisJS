19745796b23aaef06d50b1e9b8fee1eb8c52a508
Trevor <trevorblythe82@gmail.com>
2022-06-28 19:55:11 -0500

    updated the main lib file

--- Mentis.js
+++ Mentis.js
@@ -623,13 +623,7 @@ NOT like this:
 */
 
 	class ConvLayer {
-		constructor(inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1, padding = 0) {
-			if (padding != 0) {
-				throw (
-					'Dear user, I have not implemented padding yet.. set it to zero to avoid this message. ' +
-					'Star the project if you want me to complete it. or send me 5 bucks ill do it right now.'
-				);
-			}
+		constructor(inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1) {
 			this.lr = 0.01; //learning rate, this will be set by the Net object
 
 			this.filters = filters; //the amount of filters
@@ -639,19 +633,16 @@ NOT like this:
 			this.filterWidth = filterWidth;
 			this.filterHeight = filterHeight;
 			this.stride = stride;
-			this.padding = padding;
 			this.filterw = new Float32Array(filters * inDepth * filterWidth * filterHeight);
 			this.filterws = new Float32Array(filters * inDepth * filterWidth * filterHeight);
 			this.trainIterations = 0;
-			this.outData = new Float32Array(
-				Math.ceil((inWidth - filterWidth + 2 * padding + 1) / stride) * Math.ceil((inHeight - filterHeight + 2 * padding + 1) / stride) * this.filters
-			);
+			this.outData = new Float32Array(Math.ceil((inWidth - filterWidth + 1) / stride) * Math.ceil((inHeight - filterHeight + 1) / stride) * this.filters);
 			this.inData = new Float32Array(inWidth * inHeight * inDepth);
 			this.inData.fill(0); //to prevent mishap
 			this.costs = new Float32Array(inWidth * inHeight * inDepth);
 			// this.b = new Float32Array(this.outData.length);  bias in a conv layer is dumb
 			// this.bs = new Float32Array(this.outData.length);
-			if (this.filterWidth > inWidth + padding || this.filterHeight > inHeight + padding) {
+			if (this.filterWidth > inWidth || this.filterHeight > inHeight) {
 				throw 'Conv layer error: filters cannot be bigger than the input';
 			}
 			//init random weights
@@ -688,11 +679,7 @@ NOT like this:
 		}
 
 		outSizeDimensions() {
-			return [
-				Math.ceil((this.inWidth - this.filterWidth + 2 * this.padding + 1) / this.stride),
-				Math.ceil((this.inHeight - this.filterHeight + 2 * this.padding + 1) / this.stride),
-				this.filters,
-			];
+			return [Math.ceil((this.inWidth - this.filterWidth + 1) / this.stride), Math.ceil((this.inHeight - this.filterHeight + 1) / this.stride), this.filters];
 		}
 
 		forward(inData) {
@@ -829,7 +816,7 @@ NOT like this:
 		}
 
 		static load(json) {
-			//inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1, padding = 0
+			//inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1,
 			let saveObject = JSON.parse(json);
 			let layer = new ConvLayer(
 				saveObject.inWidth,
@@ -838,8 +825,7 @@ NOT like this:
 				saveObject.filterWidth,
 				saveObject.filterHeight,
 				saveObject.filters,
-				saveObject.stride,
-				saveObject.padding
+				saveObject.stride
 			);
 			for (var i = 0; i < layer.filterw.length; i++) {
 				layer.filterw[i] = saveObject.filterw[i];
@@ -858,13 +844,7 @@ NOT like this:
 
 {
 	class DeconvLayer {
-		constructor(inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1, padding = 0) {
-			if (padding != 0) {
-				throw (
-					'Dear user, I have not implemented padding yet.. set it to zero to avoid this message. ' +
-					'Star the project if you want me to complete it. or send me 5 bucks ill do it right now.'
-				);
-			}
+		constructor(inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1) {
 			this.lr = 0.01; //learning rate, this will be set by the Net object
 
 			this.filters = filters; //the amount of filters
@@ -874,13 +854,10 @@ NOT like this:
 			this.filterWidth = filterWidth;
 			this.filterHeight = filterHeight;
 			this.stride = stride;
-			this.padding = padding;
 			this.filterw = new Float32Array(filters * inDepth * filterWidth * filterHeight);
 			this.filterws = new Float32Array(filters * inDepth * filterWidth * filterHeight);
 			this.trainIterations = 0;
-			this.inData = new Float32Array(
-				Math.ceil((inWidth - filterWidth + 2 * padding + 1) / stride) * Math.ceil((inHeight - filterHeight + 2 * padding + 1) / stride) * this.filters
-			);
+			this.inData = new Float32Array(Math.ceil((inWidth - filterWidth + 1) / stride) * Math.ceil((inHeight - filterHeight + 1) / stride) * this.filters);
 			this.outData = new Float32Array(inWidth * inHeight * inDepth);
 			this.inData.fill(0); //to prevent mishap
 			this.outData.fill(0); //to prevent mishap
@@ -888,7 +865,7 @@ NOT like this:
 			// this.b = new Float32Array(this.outData.length);  bias in a conv layer is dumb
 			// this.bs = new Float32Array(this.outData.length);
 			this.accessed = new Float32Array(this.inData.length).fill(1);
-			if (this.filterWidth > inWidth + padding || this.filterHeight > inHeight + padding) {
+			if (this.filterWidth > inWidth || this.filterHeight > inHeight) {
 				throw 'Conv layer error: filters cannot be bigger than the input';
 			}
 			//init random weights
@@ -925,11 +902,7 @@ NOT like this:
 		}
 
 		inSizeDimensions() {
-			return [
-				Math.ceil((this.inWidth - this.filterWidth + 2 * this.padding + 1) / this.stride),
-				Math.ceil((this.inHeight - this.filterHeight + 2 * this.padding + 1) / this.stride),
-				this.filters,
-			];
+			return [Math.ceil((this.inWidth - this.filterWidth + 1) / this.stride), Math.ceil((this.inHeight - this.filterHeight + 1) / this.stride), this.filters];
 		}
 
 		forward(inData) {
@@ -1082,7 +1055,7 @@ NOT like this:
 		}
 
 		static load(json) {
-			//inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1, padding = 0
+			//inWidth, inHeight, inDepth, filterWidth, filterHeight, filters = 3, stride = 1,
 			let saveObject = JSON.parse(json);
 			let layer = new DeconvLayer(
 				saveObject.inWidth,
@@ -1091,8 +1064,7 @@ NOT like this:
 				saveObject.filterWidth,
 				saveObject.filterHeight,
 				saveObject.filters,
-				saveObject.stride,
-				saveObject.padding
+				saveObject.stride
 			);
 			for (var i = 0; i < layer.filterw.length; i++) {
 				layer.filterw[i] = saveObject.filterw[i];
@@ -1232,7 +1204,7 @@ NOT like this:
 			this.costs.fill(0);
 
 			let geterr = (ind) => {
-				expected[ind] - this.outData[ind];
+				return expected[ind] - this.outData[ind];
 			};
 			if (!expected) {
 				geterr = (ind) => {
@@ -1697,6 +1669,138 @@ NOT like this:
 
 	Ment.MaxPoolLayer = MaxPoolLayer;
 	Ment.MaxPool = MaxPoolLayer;
+}
+
+{
+	class PaddingLayer {
+		constructor(inWidth, inHeight, inDepth, pad, padwith) {
+			pad = pad | 2;
+			padwith = padwith | 0;
+			this.inData = new Float32Array(inWidth * inHeight * inDepth);
+			this.outData = new Float32Array((inWidth + pad * 2) * (inHeight + pad * 2) * inDepth);
+			this.pad = pad;
+			this.inWidth = inWidth;
+			this.inHeight = inHeight;
+			this.inDepth = inDepth;
+			this.padwith = padwith;
+			this.outData.fill(padwith);
+			this.inData.fill(0);
+			this.costs = new Float32Array(this.inData.length);
+			this.costs.fill(0);
+		}
+
+		// const handler = {
+		// 	get: function (obj, prop) {
+		// 		prop = Math.floor((prop % (inHeight * inWidth)) / inHeight + 1) * padding * 2 + -padding + padding * (inWidth + padding * 2) + parseFloat(prop);
+		// 		return obj[prop];
+		// 	},
+		// 	set: function (obj, prop, value) {
+		// 		prop = Math.floor((prop % (inHeight * inWidth)) / inHeight + 1) * padding * 2 + -padding + padding * (inWidth + padding * 2) + parseFloat(prop);
+		// 		obj[prop] = value;
+		// 	},
+		// };
+
+		forward(inData) {
+			if (inData) {
+				if (inData.length != this.inSize()) {
+					throw inputError(this, inData);
+				}
+				//Fun fact: the fastest way to copy an array in javascript is to use a For Loop
+				for (var i = 0; i < inData.length; i++) {
+					this.inData[i] = inData[i];
+				}
+			}
+			this.outData.fill(this.padwith);
+
+			for (var i = 0; i < this.inDepth; i++) {
+				for (var j = 0; j < this.inHeight; j++) {
+					for (var h = 0; h < this.inWidth; h++) {
+						let prop = i * this.inHeight * this.inWidth + j * this.inWidth + h;
+						this.outData[
+							(j + 1) * this.pad * 2 +
+								-this.pad +
+								this.pad * (this.inWidth + this.pad * 2) +
+								prop +
+								i * ((this.inWidth + this.pad * 2) * this.pad) * 2 +
+								i * (this.inHeight * this.pad) * 2
+						] = this.inData[prop];
+					}
+				}
+			}
+		}
+
+		backward(expected) {
+			let loss = 0;
+			this.costs.fill(0);
+
+			let geterr = (ind) => {
+				return expected[ind] - this.outData[ind];
+			};
+			if (!expected) {
+				geterr = (ind) => {
+					return this.nextLayer.costs[ind];
+				};
+				if (this.nextLayer == undefined) {
+					throw 'error backproping on an unconnected layer with no expected parameter input';
+				}
+			}
+
+			for (var i = 0; i < this.inDepth; i++) {
+				for (var j = 0; j < this.inHeight; j++) {
+					for (var h = 0; h < this.inWidth; h++) {
+						let prop = i * this.inHeight * this.inWidth + j * this.inWidth + h;
+						let err = geterr(
+							(j + 1) * this.pad * 2 +
+								-this.pad +
+								this.pad * (this.inWidth + this.pad * 2) +
+								prop +
+								i * ((this.inWidth + this.pad * 2) * this.pad) * 2 +
+								i * (this.inHeight * this.pad) * 2
+						);
+						loss += err;
+						this.costs[prop] = err;
+					}
+				}
+			}
+			return loss;
+		}
+
+		inSize() {
+			return this.inData.length;
+		}
+
+		outSize() {
+			return this.outData.length;
+		}
+
+		save() {
+			// we cant see the length of arrays after saving them in JSON
+			// for some reason so we are adding temp variables so we know
+			// the sizes;
+			let ret = JSON.stringify(this, function (key, value) {
+				//here we define what we need to save
+				if (key == 'inData' || key == 'outData' || key == 'costs' || key == 'nextLayer' || key == 'previousLayer' || key == 'pl') {
+					return undefined;
+				}
+
+				return value;
+			});
+
+			return ret;
+		}
+		//hey if your enjoying my library contact me trevorblythe82@gmail.com
+
+		static load(json) {
+			let saveObject = JSON.parse(json);
+			let layer = new PaddingLayer(saveObject.inWidth, saveObject.inHeight, saveObject.inDepth, saveObject.pad, saveObject.padwith);
+			return layer;
+		}
+	}
+
+	Ment.PaddingLayer = PaddingLayer;
+	Ment.PadLayer = PaddingLayer;
+	Ment.Padding = PaddingLayer;
+	Ment.Pad = PaddingLayer;
 }
 
 {
