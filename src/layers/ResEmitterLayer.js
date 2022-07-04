c163bc01c4a9d2c31f367b9511ed8efcaf73309c
Trevor Blythe <trevorblythe82@gmail.com>
2022-07-04 18:05:10 -0500

    bug

--- src/layers/ResEmitterLayer.js
+++ src/layers/ResEmitterLayer.js
@@ -78,7 +78,7 @@
 
 			let ret = JSON.stringify(this, function (key, value) {
 				//here we define what we need to save
-				if (key == 'receiver' || key == 'inData' || key == 'outData' || key == 'costs' || key == 'nextLayer' || key == 'previousLayer') {
+				if (key == 'receiver' || key == 'pl' || key == 'inData' || key == 'outData' || key == 'costs' || key == 'nextLayer' || key == 'previousLayer' || key == 'emitter') {
 					return undefined;
 				}
 
--- src/layers/ResReceiverLayer.js
+++ src/layers/ResReceiverLayer.js
@@ -98,7 +98,7 @@
 		save() {
 			let ret = JSON.stringify(this, function (key, value) {
 				//here we define what we need to save
-				if (key == 'emitter' || key == 'inData' || key == 'outData' || key == 'costs' || key == 'nextLayer' || key == 'previousLayer') {
+				if (key == 'emitter' || key == 'pl' || key == 'receiver' || key == 'inData' || key == 'outData' || key == 'costs' || key == 'nextLayer' || key == 'previousLayer') {
 					return undefined;
 				}
 
